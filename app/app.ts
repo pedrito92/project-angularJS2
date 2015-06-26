15ba60d1f1ac22c8c7dde5b6ce784ff94be0199d
Maelle Le Pape <maelle@macbook-air-de-maelle.home>
2015-06-26 11:51:20 +0200

    delete old sources

--- app/app.ts
+++ app/app.ts
@@ -1,58 +1,20 @@
 /// <reference path='../typings/tsd.d.ts' />
-import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
-import {Deezer} from 'services/deezer';
-import {Player} from 'components/player';
+import {Component, View, bootstrap} from 'angular2/angular2';
 
 @Component({
   selector: 'app',
-  appInjector: [Deezer]
+  //appInjector: [Deezer]
 })
 
 @View({
 	templateUrl: 'templates/main.html',
-	directives: [Player, NgFor]
+	//directives: [Player, NgFor]
 })
 
 class App {
-	sources: Array<Object>;
-	deezer: Deezer;
-  	user: Array<Object>;
-  	playlist: String;
-  	data: String;
 
-	constructor(deezer: Deezer) {
+	constructor() {
 		console.log("I work !");
-
-		this.sources = [
-			{ src: 'http://www.noiseaddicts.com/samples_1w72b820/2545.mp3', type: 'audio/mp3' },
-			{ src: 'http://www.noiseaddicts.com/samples_1w72b820/279.mp3', type: 'audio/mp3' }
-		];
-
-		this.deezer = deezer;
-
-		this.deezer.searchPlaylist('06Mart22').then(response => {
-			console.log(response);
-			if (response != null) {
-				this.data = "Data received. Take a look in the console.";
-				//this.user = response; // This first function is called if promise is fullfilled
-			}
-		}, response => {
-			console.warn('research failed'); // This second function is called if promise is rejected
-		});
-		
-		//this.get Playlist();
-	}
-		   
-	getPlaylist(){
-	  	this.playlist = this.deezer.getPlaylist(this.user.name).then(response => {
-	 	    console.log(response);
- 
-		    //this.user = respons e; // This first function is called if promise is fullfilled
-		}, response => {
-     		console.warn('Playlist loading failed'); // This second function is called if promise is rejected
-    	});
-		
-		console.log(this.playlist);
 	}
 }
 
